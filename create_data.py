bf06736a05848f55159a593cda2695b37a128b68
Aaron Jaech <ajaech@cs.washington.edu>
2017-01-19 16:47:29 -0800

    switch from usernames to subreddits

--- create_data.py
+++ create_data.py
@@ -1,5 +1,4 @@
 import bz2
-import code
 import json
 import random
 import re
@@ -8,9 +7,9 @@ from nltk import tokenize
 
 f2 = '/g/ssli/data/reddit/reddit_data_2007_2015/2015/RC_2015-04.bz2'
 
-outfile = bz2.BZ2File('/s0/ajaech/reddit.tsv', 'w')
+outfile = bz2.BZ2File('/s0/ajaech/reddit.tsv.bz2', 'w')
 
-def Extract(fname, year):
+def Extract(fname):
 
   count = 0
   with bz2.BZ2File(fname, 'r') as f:
@@ -19,18 +18,18 @@ def Extract(fname, year):
       text = data['body']
       if text == '[deleted]':
         continue
-      author = data['author']
+      subreddit = data['subreddit']
 
       sentences = tokenize.sent_tokenize(text)
       for sentence in sentences:
         re.sub(ur'http://\S*', '<URL>', sentence)
         words = ' '.join(tokenize.word_tokenize(sentence.lower()))
         
-        fields = u'\t'.join((author, words))
+        fields = u'\t'.join((subreddit, words))
         line = u'{0}\n'.format(fields)
         outfile.write(line.encode('utf8'))
         break
 
-Extract(f2, 2015)
+Extract(f2)
 
 outfile.close()
