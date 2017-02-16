bugfix in softmax adaptation

model.py
@@ -87,17 +87,14 @@ class HyperCell(rnn_cell.RNNCell):
     return self._num_units
 
   def __call__(self, inputs, state, scope=None, reuse=None):
-    """Long short-term memory cell (LSTM)."""
-
     with vs.variable_scope("hyper_lstm_cell", reuse=reuse):
       # Parameters of gates are concatenated into one multiply for efficiency.
       c, h = state
       adapted = _linear([inputs, h], 4 * self._num_units, True, scope=scope)
 
       if self.hyper_adapt:
-        adaptation_coeff = tf.nn.relu(
-          tf.matmul(self.context_embed, self.adaptation_weights) 
-          + self.adaptation_bias)
+        adaptation_coeff = (tf.matmul(self.context_embed, self.adaptation_weights)
+                            + self.adaptation_bias)
         adapted = tf.mul(adaptation_coeff, adapted)
         
       if self.mikolov_adapt:
@@ -112,7 +109,6 @@ class HyperCell(rnn_cell.RNNCell):
       new_h = self._activation(new_c) * tf.sigmoid(o)
 
       new_state = rnn_cell.LSTMStateTuple(new_c, new_h)
-
       return new_h, new_state
 
 
@@ -156,22 +152,20 @@ class BaseModel(object):
         context_bias = tf.get_variable('context_bias', [params.context_embed_size])
         
         self.final_context_embed = tf.nn.tanh(tf.matmul(context_embeds, context_mlp) + 
-                                              context_bias)
-      
-    if params.use_softmax_adaptation:
-      self._word_embeddings = tf.get_variable(
-        'word_embeddings', [self.vocab_size, self._embedding_dims +
-                            params.context_embed_size])
-    else:
-      self._word_embeddings = tf.get_variable(
-        'word_embeddings', [self.vocab_size, self._embedding_dims])
+                                              context_bias)      
+    self._word_embeddings = tf.get_variable(
+      'word_embeddings', [self.vocab_size, self._embedding_dims])
 
     self._inputs = tf.nn.embedding_lookup(self._word_embeddings, self.x)
 
-    if params.use_softmax_adaptation:  # chop off the context embedding part
-      self._inputs = self._inputs[:, :, params.context_embed_size:]
-
     self.base_bias = tf.get_variable('base_bias', [self.vocab_size])
+    # optionally concatenate the context embeddings to the outputs
+    if params.use_softmax_adaptation:
+      self.softmax_adapter = tf.get_variable(
+        'softmax_adapter', [self.final_context_embed.get_shape()[1], self.vocab_size])
+      self.adapted_bias = tf.reduce_sum(tf.matmul(
+        self.final_context_embed, self.softmax_adapter), 0)
+      self.base_bias += self.adapted_bias
 
     self.dropout_keep_prob = tf.placeholder_with_default(1.0, (), name='keep_prob')
     
@@ -180,11 +174,10 @@ class BaseModel(object):
     sz = [params.batch_size, self.max_length]
     self._mask = tf.select(indicator, tf.ones(sz), tf.zeros(sz))
 
-  def OutputHelper(self, outputs, linear_proj, params, use_nce_loss=True):
+  def OutputHelper(self, outputs, params, use_nce_loss=True):
     reshaped_outputs = tf.reshape(outputs, [-1, outputs.get_shape()[-1].value])
-    projected = tf.matmul(reshaped_outputs, linear_proj)
 
-    proj_out =  tf.reshape(projected, [outputs.get_shape()[0].value,
+    proj_out =  tf.reshape(reshaped_outputs, [outputs.get_shape()[0].value,
                                        outputs.get_shape()[1].value, -1])
 
     if use_nce_loss:
@@ -229,8 +222,8 @@ class BaseModel(object):
       reshaped_outputs = tf.concat(1, [replicated, reshaped_outputs])
 
     reshaped_mask = tf.reshape(self._mask, [-1])
-    
     reshaped_labels = tf.reshape(self.y, [-1])
+
     reshaped_logits = tf.matmul(
       reshaped_outputs, out_embeddings, transpose_b=True) + self.base_bias
     reshaped_loss = tf.nn.sparse_softmax_cross_entropy_with_logits(
@@ -243,7 +236,7 @@ class BaseModel(object):
 
     return masked_loss
 
-  def CreateDecodingGraph(self, cell, linear_proj, params):
+  def CreateDecodingGraph(self, cell, params):
     self.prev_word = tf.placeholder(tf.int32, (), name='prev_word')
     self.prev_c = tf.placeholder(tf.float32, [1, params.cell_size], name='prev_c')
     self.prev_h = tf.placeholder(tf.float32, [1, params.cell_size], name='prev_h')
@@ -256,10 +249,9 @@ class BaseModel(object):
     state = rnn_cell.LSTMStateTuple(self.prev_c, self.prev_h)
     with vs.variable_scope('RNN', reuse=True):
       result, (self.next_c, self.next_h) = cell(prev_embed, state)
-    projected = tf.matmul(result, linear_proj)
-    logits = tf.matmul(projected, self._word_embeddings, transpose_b=True) + self.base_bias
+    logits = tf.matmul(result, self._word_embeddings, transpose_b=True) + self.base_bias
     self.next_idx = tf.argmax(logits, 1)
-    self.temperature = tf.placeholder_with_default([0.2], [1])
+    self.temperature = tf.placeholder_with_default([1.0], [1])
     self.next_prob = tf.nn.softmax(logits / self.temperature)
 
 
@@ -273,20 +265,25 @@ class HyperModel(BaseModel):
     if params.use_mikolov_adaptation or params.use_hyper_adaptation:
       uembeds = self.final_context_embed
 
-    cell = HyperCell(params.cell_size, uembeds, mikolov_adapt=params.use_mikolov_adaptation,
+    cell = HyperCell(params.cell_size, uembeds,
+                     mikolov_adapt=params.use_mikolov_adaptation,
                      hyper_adapt=params.use_hyper_adaptation)
 
     regularized_cell = rnn_cell.DropoutWrapper(
       cell, output_keep_prob=self.dropout_keep_prob,
       input_keep_prob=self.dropout_keep_prob)
+
+    self.linear_proj = tf.get_variable(
+      'linear_proj', [params.cell_size, params.embedding_dims])
     outputs, _ = tf.nn.dynamic_rnn(regularized_cell, self._inputs, dtype=tf.float32,
                                    sequence_length=self.seq_len)
+    reshaped_outputs = tf.reshape(outputs, [-1, params.cell_size])
+    projected_outputs = tf.matmul(reshaped_outputs, self.linear_proj)
+    proj_out3d = tf.reshape(projected_outputs, [-1, params.max_len, 
+                                                params.embedding_dims])
+    self.OutputHelper(proj_out3d, params, use_nce_loss=use_nce_loss)
 
-    linear_proj = tf.get_variable(
-      'linear_proj', [params.cell_size, self._word_embeddings.get_shape()[1]])
-    self.OutputHelper(outputs, linear_proj, params, use_nce_loss=use_nce_loss)
-
-    self.CreateDecodingGraph(cell, linear_proj, params)
+    #self.CreateDecodingGraph(cell, params)
 
 
 def PrintParams(handle=sys.stdout.write):

rnnlm.py
@@ -2,6 +2,7 @@
 import argparse
 import bunch
 import collections
+import copy
 import gzip
 import json
 import logging
@@ -12,6 +13,7 @@ import pickle
 import shutil
 import sys
 import tensorflow as tf
+import time
 
 from model import HyperModel, PrintParams
 from vocab import Vocab
@@ -155,6 +157,7 @@ def Train(expdir):
     saver.restore(session, os.path.join(args.initialize, 'model.bin'))
 
   avgcost = metrics.MovingAvg(0.90)
+  start_time = time.time()
   for idx in xrange(params.iters):
     batch = dataset.GetNextBatch()
     feed_dict = GetFeedDict(batch, use_dropout=True)
@@ -162,6 +165,12 @@ def Train(expdir):
     cost, _ = session.run([model.cost, train_op], feed_dict)
 
     if idx % 40 == 0:
+      end_time = time.time()
+      time_diff = end_time - start_time
+      start_time = end_time
+      batches_per_second = time_diff / 40
+      print 'batches per second {0}'.format(batches_per_second)
+
       feed_dict = GetFeedDict(dataset.GetValBatch(), use_dropout=False)
       val_cost = session.run(model.cost, feed_dict)
       print idx, cost
@@ -205,21 +214,83 @@ def Debug(expdir):
   base_bias = session.run(model.base_bias)
   Process(base_bias.T, 'Baseline')
 
-  uword = model._word_embeddings[:, :params.user_embedding_size]
-  subreddit = tf.placeholder(tf.int32, ())
-  scores = tf.matmul(uword, tf.expand_dims(model._user_embeddings[subreddit, :], 1))
+  uword = model._word_embeddings[:, :params.context_embed_size]
+  m = model
+  code.interact(local=locals())
+  
+  scores = tf.matmul(uword, tf.expand_dims(model.final_context_embed, 1))
 
-  for subname in ['exmormon', 'AskMen', 'AskWomen', 'Music', 'aww',
-                  'dogs', 'cats', 'worldnews', 'tifu', 'books', 'WTF',
-                  'RealGirls', 'relationships', 'Android']:
-    s = session.run(scores, {subreddit: username_vocab[subname]})
+  subnames = ['en', 'es', 'pt', 'de', 'it']
+  for subname in subnames:
+    s = session.run(scores, {model.context_placeholders['lang']:
+                             context_vocabs['lang'][subname]})
     Process(s, subname)
 
+
+class BeamItem(object):
+  
+  def __init__(self, prev_word, prev_c, prev_h):
+    self.log_probs = [0.0]
+    self.words = [prev_word]
+    self.prev_c = prev_c
+    self.prev_h = prev_h
+
+  def Update(self, log_prob, new_word, new_c, new_h):
+    self.prev_c = new_c
+    self.prev_h = new_h
+    self.words.append(new_word)
+    self.log_probs.append(log_prob)
+
+  def Cost(self):
+    return sum(self.log_probs)
+
+
+def BeamSearch(expdir):
+  saver.restore(session, os.path.join(expdir, 'model.bin'))
+
+  varname = 'lang'
+  subname = 'en'
+
+  beam_size = 12
+  current_word = '<S>'
+  prevstate_h = np.zeros((1, params.cell_size))
+  prevstate_c = np.zeros((1, params.cell_size))
+  beam_items = []
+
+  # initalize beam
+  beam_items.append(BeamItem('<S>', prevstate_h, prevstate_c))
+
+  for i in xrange(30):
+    new_beam_items = []
+    for beam_item in beam_items:
+      feed_dict = {
+        model.prev_word: vocab[beam_item.words[-1]],
+        model.prev_c: beam_item.prev_c,
+        model.prev_h: beam_item.prev_h
+      }
+      for context_var in params.context_vars:
+        placeholder = model.context_placeholders[context_var]
+        if context_var == varname:
+          feed_dict[placeholder] = np.array([context_vocabs[context_var][subname]])
+        else:
+          feed_dict[placeholder] = np.array([0])
+
+      a = session.run([model.next_prob, model.next_c, model.next_h], feed_dict)
+      current_prob, prevstate_h, prevstate_c = a
+      top_entries = np.argsort(current_prob)[0, -beam_size:]
+      top_values = current_prob[0, top_entries]
+      for top_entry, top_value in zip(top_entries, top_values):
+        new_beam = copy.deepcopy(beam_item)
+        new_beam.Update(-np.log(top_value), vocab[top_entry], prevstate_c, prevstate_h)
+        new_beam_items.append(new_beam)
+      new_beam_items = sorted(new_beam_items, key=lambda x: x.Cost())
+      beam_items = new_beam_items[:5]
+  code.interact(local=locals())
 
 def Greedy(expdir):
   saver.restore(session, os.path.join(expdir, 'model.bin'))
 
-  def Process(subname):
+  def Process(varname, subname):
     current_word = '<S>'
     prevstate_h = np.zeros((1, params.cell_size))
     prevstate_c = np.zeros((1, params.cell_size))
@@ -234,7 +305,10 @@ def Greedy(expdir):
       }
       for context_var in params.context_vars:
         placeholder = model.context_placeholders[context_var]
-        feed_dict[placeholder] = np.array([0])
+        if context_var == varname:
+          feed_dict[placeholder] = np.array([context_vocabs[context_var][subname]])
+        else:
+          feed_dict[placeholder] = np.array([1])
 
       a = session.run([model.next_prob, model.next_c, model.next_h], feed_dict)
       current_prob, prevstate_h, prevstate_c = a
@@ -245,18 +319,17 @@ def Greedy(expdir):
       words.append(current_word)
       if '</S>' in current_word:
         break
-    
     ppl = np.exp(np.mean(log_probs))
     return ppl, SEPERATOR.join(words)
     
   sample_list = ['AskWomen', 'AskMen', 'exmormon', 'Music', 'worldnews',
                  'GoneWild', 'tifu', 'WTF', 'AskHistorians', 'hockey']
-  sample_list = ['en', 'fr', 'pt', 'es', 'und']
+  sample_list = ['en', 'fr', 'pt', 'es', 'eu']
 
   for n in sample_list:
     print '~~~{0}~~~'.format(n)
     for _ in range(5):
-      ppl, sentence = Process(n)
+      ppl, sentence = Process('lang', n)
       print '{0:.2f}\t{1}'.format(ppl, sentence)
 
 
@@ -320,7 +393,10 @@ def Eval(expdir):
                                        feed_dict)
 
     lens = feed_dict[model.seq_len]
-    unames = feed_dict[model.context_placeholders['lang']]
+    if hasattr(model, 'context_placeholders'):
+      unames = feed_dict[model.context_placeholders['lang']]
+    else: 
+      unames = ['None'] * len(lens)
 
     for length, uname, sentence_cost in zip(lens, unames, sentence_costs):
       results.append({'length': length, 'uname': context_vocabs['lang'][uname],
@@ -346,8 +422,8 @@ if args.mode == 'classify':
   Classify(args.expdir)
 
 if args.mode == 'debug':
-  #Debug(args.expdir)
-  Greedy(args.expdir)
+  Debug(args.expdir)
+  #BeamSearch(args.expdir)
 
 if args.mode == 'dump':
   DumpEmbeddings(args.expdir)