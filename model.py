fix softmax adaptation bug

model.py
@@ -1,7 +1,6 @@
 import sys
 import numpy as np
 import tensorflow as tf
-import nn_impl
 from tensorflow.python.ops import variable_scope as vs
 from tensorflow.python.ops import rnn_cell
 
@@ -156,19 +155,21 @@ class BaseModel(object):
         context_bias = tf.get_variable('context_bias', [params.context_embed_size])
         
         self.final_context_embed = tf.nn.tanh(tf.matmul(context_embeds, context_mlp) + 
-                                              context_bias)      
-    self._word_embeddings = tf.get_variable(
-      'word_embeddings', [self.vocab_size, self._embedding_dims])
+                                              context_bias)
 
+    if params.use_softmax_adaptation:
+      self._word_embeddings = tf.get_variable(
+        'word_embeddings', [self.vocab_size, self._embedding_dims + params.context_embed_sizes[0]])
+    else:
+      self._word_embeddings = tf.get_variable(
+        'word_embeddings', [self.vocab_size, self._embedding_dims])
+      
     self._inputs = tf.nn.embedding_lookup(self._word_embeddings, self.x)
 
-    self.base_bias = tf.get_variable('base_bias', [self.vocab_size])
-    # optionally concatenate the context embeddings to the outputs
     if params.use_softmax_adaptation:
-      self.softmax_adapter = tf.get_variable(
-        'softmax_adapter', [self.final_context_embed.get_shape()[1], self.vocab_size])
-      self.adapted_bias = tf.reduce_sum(tf.matmul(
-        self.final_context_embed, self.softmax_adapter), 0)
+      self._inputs = self._inputs[:, :, params.context_embed_size:]
+
+    self.base_bias = tf.get_variable('base_bias', [self.vocab_size])
 
     self.dropout_keep_prob = tf.placeholder_with_default(1.0, (), name='keep_prob')
     
@@ -182,10 +183,22 @@ class BaseModel(object):
       proj_out =  tf.reshape(reshaped_outputs, [self._mask.get_shape()[0].value,
                                                 self._mask.get_shape()[1].value, -1])
 
+      # add in the context embeddings
+      if params.use_softmax_adaptation:
+        packed_context_embed = tf.pack([self.final_context_embed] * params.max_len, 1)
+        proj_out = tf.concat(2, [packed_context_embed, proj_out])
+
       losses = self.DoNCE(proj_out, self._word_embeddings, num_sampled=params.nce_samples,
                           softmax_adaptation=params.use_softmax_adaptation)
       masked_loss = tf.mul(losses, self._mask)
     else:
+
+      # add in the context embeddings
+      if params.use_softmax_adaptation:
+        packed_context_embed = tf.pack([self.final_context_embed] * params.max_len, 1)
+        reshaped_context = tf.reshape(packed_context_embed, [tf.shape(reshaped_outputs)[0], -1])
+        reshaped_outputs = tf.concat(1, [reshaped_context, reshaped_outputs])
+
       masked_loss = self.ComputeLoss(reshaped_outputs, self._word_embeddings)
       self.masked_loss = masked_loss
 
@@ -200,12 +213,6 @@ class BaseModel(object):
     w_list = tf.unpack(weights, axis=1)
     losses = []
     
-    context_embedding=None
-    adapted_bias = None
-    if softmax_adaptation:
-      context_embedding = self.final_context_embed
-      adapted_bias = self.softmax_adapter
-
     for w, y in zip(tf.unpack(weights, axis=1), tf.split(1, self.max_length, self.y)):
       sampled_values = tf.nn.fixed_unigram_candidate_sampler(
         true_classes=y,
@@ -215,11 +222,10 @@ class BaseModel(object):
         range_max=self.vocab_size,
         unigrams=self.unigram_probs
       )
-      nce_loss = nn_impl.sampled_softmax_loss(out_embeddings, self.base_bias, 
-                                              y, w, num_sampled, self.vocab_size, 
-                                              sampled_values=sampled_values,
-                                              context_embeddings=context_embedding,
-                                              adapted_bias=adapted_bias)  
+      nce_loss = tf.nn.sampled_softmax_loss(out_embeddings, self.base_bias, 
+                                            w, y, num_sampled, self.vocab_size, 
+                                            sampled_values=sampled_values)
+                                          
       losses.append(nce_loss)
     return tf.pack(losses, 1)
 
@@ -248,22 +254,26 @@ class BaseModel(object):
     self.prev_word = tf.placeholder(tf.int32, (), name='prev_word')
     self.prev_c = tf.placeholder(tf.float32, [1, params.cell_size], name='prev_c')
     self.prev_h = tf.placeholder(tf.float32, [1, params.cell_size], name='prev_h')
-    self.temperature = tf.placeholder_with_default([0.7], [1])
+    self.temperature = tf.placeholder_with_default([0.6], [1])
 
     # lookup embedding
     prev_embed = tf.nn.embedding_lookup(self._word_embeddings, self.prev_word)
     prev_embed = tf.expand_dims(prev_embed, 0)
     
+    if params.use_softmax_adaptation:
+      prev_embed = prev_embed[:, params.context_embed_size:]
+
     # one iteration of recurrent layer
     state = rnn_cell.LSTMStateTuple(self.prev_c, self.prev_h)
     with vs.variable_scope('RNN', reuse=True):
       result, (self.next_c, self.next_h) = cell(prev_embed, state)
       proj_result = tf.matmul(result, self.linear_proj)
+      
+    if params.use_softmax_adaptation:
+      proj_result = tf.concat(1, [self.final_context_embed, proj_result])
 
     # softmax layer
     bias = self.base_bias
-    if params.use_softmax_adaptation:
-      bias += self.adapted_bias
     logits = tf.matmul(proj_result, self._word_embeddings, transpose_b=True) + bias
     self.next_idx = tf.argmax(logits, 1)
     self.next_prob = tf.nn.softmax(logits / self.temperature)

nn_impl.py
@@ -21,6 +21,7 @@ from __future__ import print_function
 import code
 import math
 
+import tensorflow as tf
 from tensorflow.python.framework import constant_op
 from tensorflow.python.framework import dtypes
 from tensorflow.python.framework import ops
@@ -142,11 +143,23 @@ def _compute_sampled_logits(weights,
         weights, all_ids, partition_strategy=partition_strategy)
     all_b = embedding_ops.embedding_lookup(biases, all_ids)
 
+    true_adapt = None
+    sampled_adapt = None
     if context_embeddings is not None:
-      adapted = embedding_ops.embedding_lookup(
-        array_ops.transpose(adapted_bias), all_ids)
-      prod = math_ops.matmul(context_embeddings, adapted, transpose_b=True)
-      all_b += math_ops.reduce_sum(prod, 0)
+      adapted_bias_t = array_ops.transpose(adapted_bias)
+      # context_embeddings is [batch_size x context_embed_size]
+      true_adapt_embed = embedding_ops.embedding_lookup(
+        adapted_bias_t, labels_flat)
+      sampled_adapt_embed = embedding_ops.embedding_lookup(
+        adapted_bias_t, sampled)
+      # prod is [batch_size * num_ids]
+      true_adapt = tf.matmul(context_embeddings, true_adapt_embed, 
+                             transpose_b=True)
+      sampled_adapt = tf.matmul(context_embeddings, sampled_adapt_embed,
+                                transpose_b=True)
+      vals = [tf.reduce_min(true_adapt), tf.reduce_max(true_adapt),
+              tf.reduce_min(sampled_adapt), tf.reduce_max(sampled_adapt)]
+      sampled_adapt = tf.Print(sampled_adapt, vals)
 
     # true_w shape is [batch_size * num_true, dim]
     # true_b is a [batch_size * num_true] tensor
@@ -164,11 +177,13 @@ def _compute_sampled_logits(weights,
         array_ops.reshape(true_w, new_true_w_shape))
     # We want the row-wise dot plus biases which yields a
     # [batch_size, num_true] tensor of true_logits.
-    dots_as_matrix = array_ops.reshape(row_wise_dots,
-                                       array_ops.concat(0, [[-1], dim]))
-    true_logits = array_ops.reshape(_sum_rows(dots_as_matrix), [-1, num_true])
-    true_b = array_ops.reshape(true_b, [-1, num_true])
+    dots_as_matrix = tf.reshape(row_wise_dots,
+                                array_ops.concat(0, [[-1], dim]))
+    true_logits = tf.reshape(_sum_rows(dots_as_matrix), [-1, num_true])
+    true_b = tf.reshape(true_b, [-1, num_true])
     true_logits += true_b
+    if true_adapt is not None:
+      true_logits += tf.expand_dims(tf.reduce_sum(true_adapt, 1), 1)
 
     # Lookup weights and biases for sampled labels.
     #   sampled_w shape is [num_sampled, dim]
@@ -183,6 +198,8 @@ def _compute_sampled_logits(weights,
     # Apply X*W'+B, which yields [batch_size, num_sampled]
     sampled_logits = math_ops.matmul(
         inputs, sampled_w, transpose_b=True) + sampled_b
+    if sampled_adapt is not None:
+      sampled_logits += sampled_adapt
 
     if remove_accidental_hits:
       acc_hits = candidate_sampling_ops.compute_accidental_hits(

rnnlm.py
@@ -180,7 +180,7 @@ def Train(expdir):
       logging.info({'iter': idx, 'cost': avgcost.Update(cost),
                     'rawcost': cost, 'valcost': val_cost})
 
-      if idx % 1000 == 0:
+      if idx % 100 == 0:
         saver.save(session, os.path.join(expdir, 'model.bin'))
 
 
@@ -199,6 +199,10 @@ def Debug(expdir):
   saver.restore(session, os.path.join(expdir, 'model.bin'))
   PrintParams()
 
+  uword = model._word_embeddings[:, :params.context_embed_sizes[0]]
+  subreddit = tf.placeholder(tf.int32, ())
+  scores = tf.matmul(uword, tf.expand_dims(model.context_embeddings['subreddit'][subreddit, :], 1))
+
   def Process(s, subname):
     s = np.squeeze(s.T)
     vals = s.argsort()
@@ -209,10 +213,10 @@ def Debug(expdir):
     print ' '.join(topwords)
     
   #subnames = ['en', 'es', 'pt', 'de', 'it']
-  subnames = ['exmormon', 'askwomen', 'todayilearned', 'nfl', 'pics', 'videos', 'worldnews']
+  subnames = ['exmormon', 'AskWomen', 'todayilearned', 'nfl', 'pics', 'videos', 'worldnews',
+              'math', 'Seattle', 'science', 'WTF', 'malefashionadvice', 'programming']
   for subname in subnames:
-    s = session.run(model.adapted_bias, {model.context_placeholders['subreddit']:
-                                         np.expand_dims(context_vocabs['subreddit'][subname], 0)})
+    s = session.run(scores, {subreddit: context_vocabs['subreddit'][subname]})
     Process(s, subname)
 
 
@@ -238,7 +242,7 @@ def BeamSearch(expdir):
   saver.restore(session, os.path.join(expdir, 'model.bin'))
 
   varname = 'subreddit'
-  subname = 'AskReddit'
+  subname = 'worldnews'
 
   beam_size = 30
   beam_items = []
@@ -390,7 +394,7 @@ def Eval(expdir):
   total_word_count = 0
   total_log_prob = 0
   results = []
-  for pos in xrange(min(dataset.GetNumBatches(), 2000)):
+  for pos in xrange(min(dataset.GetNumBatches(), 600)):
     batch = dataset.GetNextBatch()
     feed_dict = GetFeedDict(batch)
 
@@ -427,9 +431,9 @@ if args.mode == 'classify':
   Classify(args.expdir)
 
 if args.mode == 'debug':
-  #Debug(args.expdir)
-  #Greedy(args.expdir)
-  BeamSearch(args.expdir)
+  Debug(args.expdir)
+  Greedy(args.expdir)
+  #BeamSearch(args.expdir)
 
 if args.mode == 'dump':
   DumpEmbeddings(args.expdir)